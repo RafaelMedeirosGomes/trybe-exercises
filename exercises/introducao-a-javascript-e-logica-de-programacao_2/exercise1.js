d9ac85d18afc38c6d1bc9229df07a3305f9f1435
Rafael Medeiros <62731714+RafaelMedeirosGomes@users.noreply.github.com>
2021-02-25 20:53:23 -0300

    Done exercise 3

--- exercises/introducao-a-javascript-e-logica-de-programacao_2/exercise1.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_2/exercise1.js
@@ -1,11 +1,13 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 //Exercise 1
-for (let i = 0; i < numbers.length; i++) {
-  console.log(numbers[i]);
+for (let index = 0; index < numbers.length; index++) {
+  console.log(numbers[index]);
 }
 //Exercise 2
 let sum = 0;
-for (let i = 0; i < numbers.length; i++){
-  sum += numbers[i];
+for (let index = 0; index < numbers.length; index++){
+  sum += numbers[index];
 }
 console.log(sum);
+//Exercise 3
+console.log(sum/numbers.length);
