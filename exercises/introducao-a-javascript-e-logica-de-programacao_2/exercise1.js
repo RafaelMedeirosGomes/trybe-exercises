462b03da92e8fc9377577daf11cc813608aedb93
Rafael Medeiros <62731714+RafaelMedeirosGomes@users.noreply.github.com>
2021-02-25 20:59:16 -0300

    Done ex 4

--- exercises/introducao-a-javascript-e-logica-de-programacao_2/exercise1.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_2/exercise1.js
@@ -12,3 +12,9 @@ console.log(sum);
 //Exercise 3
 let average = sum/numbers.length;
 console.log(average);
+//Exercise 4
+if(average>20){
+  console.log("valor maior que 20");
+}else{
+  comsole.log("valor menor ou igual a 20");
+}
