515cf5210052d3521af08fa88aaab94742f9027c
Rafael Medeiros <62731714+RafaelMedeirosGomes@users.noreply.github.com>
2021-02-26 18:26:13 -0300

    Fixed typos

--- exercises/introducao-a-javascript-e-logica-de-programacao_3/exercise.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_3/exercise.js
@@ -1,21 +1,25 @@
 //Random well-behaviored user input simulated
 let n = 1 + Math.round(Math.random() * 10);
 //Ex 1
+console.log("Exercício 1:");
 let myChar = "*";
 let row = "";
 for (let index = 0; index < n; index += 1) {
   row += myChar;
 }
 for (let index = 0; index < n; index += 1) {
-  //console.log(row);
+  console.log(row);
 }
 let triangle = "";
 //Ex 2
+console.log("Exercício 2:");
 for (let index = 1; index <= n; index += 1) {
   triangle += myChar;
-  //console.log(triagle);
+
+  console.log(triangle);
 }
 //Ex 3
+console.log("Exercício 3:");
 let invertedTriangle = [];
 let tempRow = "";
 for (let index = 1; index <= n; index += 1) {
@@ -29,16 +33,17 @@ for (let index = 1; index <= n; index += 1) {
   tempRow = "";
 }
 for (let index = 0; index < invertedTriangle.length; index += 1) {
-  //console.log(invertedTriangle[index]);
+  console.log(invertedTriangle[index]);
 }
 //Ex 4
+console.log("Exercício 4:");
 //fixes my number to be odd in the most crude way imaginable
 let oddNumber = n * 2 + 1;
 let base = oddNumber;
 let height = n + 1;
 let pyramid = [];
 /*
-ideia do professor é boa
+ideia do professor Oliva é boa
 use 2 loops + 1 condicional para tudo, ao inves de inventar moda
 */
 // x x * x x
@@ -48,29 +53,19 @@ use 2 loops + 1 condicional para tudo, ao inves de inventar moda
 for (let index2 = 0; index2 < height; index2 += 1) {
   let myCharsInThisLine = index2 * 2 + 1;
   let emptySpaces = base - myCharsInThisLine;
-  let emptySpacesAtEachSide = emptySpaces/2;
+  let emptySpacesAtEachSide = emptySpaces / 2;
   for (let index = 1; index <= base; index += 1) {
-    if(index <= emptySpacesAtEachSide){
+    if (index <= emptySpacesAtEachSide) {
       tempRow += " ";
-    }else if(index <= emptySpacesAtEachSide + myCharsInThisLine){
+    } else if (index <= emptySpacesAtEachSide + myCharsInThisLine) {
       tempRow += myChar;
-    }else{
+    } else {
       tempRow += " ";
     }
   }
   pyramid.push(tempRow);
-  tempRow = ""
+  tempRow = "";
 }
-for (let index = 0; index < pyramid.length; index += 1){
+for (let index = 0; index < pyramid.length; index += 1) {
   console.log(pyramid[index]);
 }
-
-/*
-Rainha:
-horizontal = H fixo
-vertical = V fixo
-(para uma das )diagonais H+V fixo
-rainha no 4-4 pega diagnonal 1-1,2-2,3-3,4-4,5-5,6-6,7-7,8-8
-rainha no 3-6 pega diagonal 1-4,2-5,3-6,4-7,5-8
-ou seja, para a outra diagonal H-V fixo
-*/
