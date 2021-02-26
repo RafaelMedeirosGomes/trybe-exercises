b8f4f80d15a0c30197a2842374f2ce247057bf20
Rafael Medeiros <62731714+RafaelMedeirosGomes@users.noreply.github.com>
2021-02-25 21:14:51 -0300

    Done 5, 6 and added a .gitignore to hide .vscode config folder that was annoying me

--- exercises/introducao-a-javascript-e-logica-de-programacao_2/exercise1.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_2/exercise1.js
@@ -1,20 +1,41 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 //Exercise 1
-for (let index = 0; index < numbers.length; index++) {
+for (let index = 0; index < numbers.length; index += 1) {
   console.log(numbers[index]);
 }
 //Exercise 2
 let sum = 0;
-for (let index = 0; index < numbers.length; index++){
+for (let index = 0; index < numbers.length; index += 1) {
   sum += numbers[index];
 }
 console.log(sum);
 //Exercise 3
-let average = sum/numbers.length;
+let average = sum / numbers.length;
 console.log(average);
 //Exercise 4
-if(average>20){
+if (average > 20) {
   console.log("valor maior que 20");
+} else {
+  console.log("valor menor ou igual a 20");
+}
+//Exercise 5
+const smallestValuePossible = -1 * Number.MAX_VALUE;
+let biggestNumber = smallestValuePossible;
+for (let index = 0; index < numbers.length; index += 1) {
+  if (numbers[index] > biggestNumber) {
+    biggestNumber = numbers[index];
+  }
+}
+console.log(biggestNumber);
+//Exercise 6
+let oddNumberCounter = 0;
+for (let index = 0; index < numbers.length; index += 1) {
+  if (numbers[index] % 2 === 1) {
+    oddNumberCounter += 1;
+  }
+}
+if(oddNumberCounter !== 0){
+  console.log(oddNumberCounter)
 }else{
-  comsole.log("valor menor ou igual a 20");
+  console.log("nenhum valor ímpar encontrado");
 }
