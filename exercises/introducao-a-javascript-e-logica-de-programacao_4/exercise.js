31887f26d14fd643cd796203d8d8167b005714b0
Rafael Medeiros <62731714+RafaelMedeirosGomes@users.noreply.github.com>
2021-03-01 13:20:30 -0300

    Done 5

--- exercises/introducao-a-javascript-e-logica-de-programacao_4/exercise.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_4/exercise.js
@@ -16,3 +16,17 @@ for(let key in info){
 for(let key in info){
   console.log(info[key]);
 }
+
+let info2 = {
+  personagem: "Tio Patinhas",
+  origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+  nota: "O último MacPatinhas",
+  recorrente: "Sim",
+};
+
+console.log(info.personagem + " e " + info2.personagem);
+console.log(info.origem + " e " + info2.origem);
+console.log(info.nota + " e " + info2.nota);
+if(info.recorrente ==="Sim" && info2.recorrente === "Sim"){
+  console.log("Ambos recorrentes");
+}
