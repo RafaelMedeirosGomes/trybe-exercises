c46ae60aa3157b62ce6a8268846a290ee84d17a0
Rafael Medeiros <62731714+RafaelMedeirosGomes@users.noreply.github.com>
2021-03-01 13:32:10 -0300

    Done 2

--- exercises/introducao-a-javascript-e-logica-de-programacao_4/exercise2.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_4/exercise2.js
@@ -1,8 +1,8 @@
 //EX 1
 function verificaPalindrome(word) {
   let count = word.length;
-  for (let index = 0; index < count-index; index += 1) {
-    if(word[index] != word[count-index-1]){
+  for (let index = 0; index < count - index; index += 1) {
+    if (word[index] != word[count - index - 1]) {
       return false;
     }
   }
@@ -10,3 +10,16 @@ function verificaPalindrome(word) {
 }
 console.log(verificaPalindrome("arara"));
 console.log(verificaPalindrome("desenvolvimento"));
+//EX 2
+function maiorNaArray(array) {
+  let biggest = -1 * Number.MAX_VALUE;
+  let index = -1;
+  for(let key in array){
+    if(array[key] > biggest){
+      biggest = array[key];
+      index = key;
+    }
+  }
+  return index;
+}
+console.log(maiorNaArray([2, 3, 6, 7, 10, 1]));
